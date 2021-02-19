25d448d685c0a7fa9c0eb814d14d5497416f53b9
GAC8WZ <marcin.grycel@pl.bosch.com>
2021-02-19 15:27:35 +0100

    proxy fix and loggin added

--- src/main/java/utils/TestInitialization.java
+++ src/main/java/utils/TestInitialization.java
@@ -1,9 +1,10 @@
 package utils;
 
 import io.restassured.RestAssured;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import org.apache.log4j.Logger;
-
-import java.io.IOException;
 
 public class TestInitialization {
 
@@ -18,13 +19,19 @@ public class TestInitialization {
         if (!isInit) {
             try {
                 LOG.info("Test execution initialization");
+                String userDirProperty = System.getProperty("user.dir");
 
-                System.setProperty("isLocalRun", Boolean.toString(System.getProperty("user.dir").contains("C:\\Users")));
+                System.setProperty("isLocalRun", Boolean.toString(userDirProperty.contains("C:\\Users") && !userDirProperty.contains("gac8wz")));
 
                 if (System.getProperty("isLocalRun").equals("false")) {
                     RestAssured.proxy("localhost", 3128);
                     RestAssured.useRelaxedHTTPSValidation();
                 }
+
+                RestAssured.filters(
+                        new RequestLoggingFilter(LogDetail.URI, true, System.out),
+                        new ResponseLoggingFilter()
+                );
 
                 isInit = true;
             } catch (Exception e) {
