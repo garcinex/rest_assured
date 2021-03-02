2a40a8a27a22e16b96f007856f50815c6d0aabe8
GAC8WZ <marcin.grycel@pl.bosch.com>
2021-03-02 15:51:41 +0100

    added TestReport.java

--- src/main/java/listener/TestListener.java
+++ src/main/java/listener/TestListener.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.Logger;
 import org.testng.*;
 import utils.TestInitialization;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,17 +15,18 @@ public class TestListener implements ITestListener {
     private static final Logger LOG = LogManager.getLogger(TestInitialization.class);
     private final List<String> passed = new ArrayList<>();
     private final List<String> failed = new ArrayList<>();
+    private final List<String> skipped = new ArrayList<>();
 
     @Override
     public void onTestStart(ITestResult iTestResult) {
         String testName = iTestResult.getTestClass().getName() + "." + iTestResult.getName();
-        LOG.info(String.format(">>> Execute test %s <<<", testName));
+        LOG.info(String.format(">>> Execute test: %s <<<", testName));
     }
 
     @Override
     public void onFinish(ITestContext iTestContext) {
-        LOG.info("ON FINISH");
-        LOG.info(String.format("PASSED: %s, FAILED: %s", passed.size(), failed.size()));
+        LOG.info("TEST RUN FINISHED AT: " + LocalDateTime.now().toString());
+        LOG.info(String.format("PASSED: %s, FAILED: %s, SKIPPED: %s", passed.size(), failed.size(), skipped.size()));
     }
 
     @Override
@@ -41,7 +43,8 @@ public class TestListener implements ITestListener {
 
     @Override
     public void onTestSkipped(ITestResult iTestResult) {
-
+        String testName = iTestResult.getTestClass().getName() + "." + iTestResult.getName();
+        skipped.add(testName);
     }
 
     @Override
@@ -51,6 +54,6 @@ public class TestListener implements ITestListener {
 
     @Override
     public void onStart(ITestContext iTestContext) {
-
+        LOG.info("TEST RUN STARTED AT: " + LocalDateTime.now().toString());
     }
 }
